165e2b7333bf0391ccd78c364399d3104fa235a8
Pepijn Kok <59015908+pepijnkok@users.noreply.github.com>
2020-02-13 13:50:08 +0100

    Create baby-steps.js

---
+++ baby-steps.js
@@ -0,0 +1,12 @@
+function sum() {
+  var total = 0;
+  var index = 1;
+
+
+  while (++index < process.agrv.length) {
+    total += Number(process.argv[index]);
+  }
+    console.log(total);
+}
+
+sum();
