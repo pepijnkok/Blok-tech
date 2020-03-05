b7acc3e72a89db1aa8c79b2b8d1888f1eebce4bb
Pepijn Kok <pepijnkok66@gmail.com>
2020-03-05 11:36:35 +0100

    commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-const ejs = requere("ejs");
+const ejs = require("ejs");
 const port = 3000;
 
 app.set("view enginge", "ejs")
 app.use(express.static("views"))
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-// app.get('/about',(req, res)=> res.send('hello'))
+app.get("/matching", (req, res) => {
+    res.render("matching.ejs", {data });
+});
 
 app.use(function(req,res) {
     res.send("404: Page not found", 404);
