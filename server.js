83c6ecfb7873de2daa2e121e0ff4522fab19d435
Pepijn Kok <pepijnkok66@gmail.com>
2020-03-05 11:59:44 +0100

    commit

--- server.js
+++ server.js
@@ -1,18 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const ejs = require("ejs");
+// const slug = require("slug");
+// const bodyParser = require("bodyParser");
 const port = 3000;
 
-app.set("view enginge", "ejs")
+
+app.set("view engine", "ejs")
 app.use(express.static("views"))
 
-app.get("/matching", (req, res) => {
-    res.render("matching.ejs", {data });
+
+var data = [
+  {
+  id: "batman",
+  title: "dark knight",
+  description: "Gotham city"
+},
+
+{
+  id: "spiderman",
+  title: "Far from home",
+  summary: "in europe"
+}  
+]
+
+app.get('/', function(req, res){
+    res.render('pages/index',{
+
+  
+
+    });
 });
 
-app.use(function(req,res) {
-    res.send("404: Page not found", 404);
+app.get('/index', function(req, res){
+    res.render('pages/index',{
+
+     
+
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/matching', function(req, res){
+    res.render('pages/matching',{
 
+   
+
+    });
+});
+
+
+
+
+app.use(function(req, res) {
+  res.send("404: Page not found", 404);
+});
+
+app.listen(port, () => console.log(`app running on port: ${port}`));
