07c4cadb9e6170b899055dadd3d938bdef0c9473
Pepijn Kok <pepijnkok66@gmail.com>
2021-02-11 14:42:20 +0100

    test

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/profile/:id', function(req, res){
 
 
 app.use(function(req, res) {
-  res.send("404: Page not found", 404);
+  res.send("4044: Page not found", 404);
 });
 
 app.listen(port, () => console.log(`app running on port: ${port}`));
