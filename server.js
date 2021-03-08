3d92dfbeb9ced8385e65f7d4c5cee0e9bad9b588
Pepijn Kok <pepijnkok66@gmail.com>
2021-03-08 15:55:52 +0100

    mongoose setup

--- server.js
+++ server.js
@@ -1,27 +1,66 @@
+const bodyParser = require ('body-parser')
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 const path = require('path')
 const PORT = 3000
-const router = require('./route/router')
-
-// MongoDB
 const mongo = require('mongodb')
+const mongoose = require('mongoose')
 
-// Dotenv
-require ('dotenv').config()
+require('dotenv').config()
+
+
+const db = mongoose.connection
+
+//mongoose connecten met de database
+mongoose.connect(process.env.DB_URI, {
+   useNewUrlParser: true, 
+   useUnifiedTopology: true
+})
+
+db.on('connected', () => { 
+   console.log('Moongoose connected')
+})
+
+
 
 // EJS setup
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 // Set de views folder
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 // Gebruik statische files vanuit de public folder
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-// Gebruik de router wanneer je op de index pagina komt
-app.use('/', router)
+
+//paginas inladen
+app.get('/', function (req, res) {
+   res.render('pages/index', {
+       title: 'Travel Date',  
+   })
+});
+
+app.get('/login', function (req, res) {
+   res.render('pages/login', {
+       title: 'login',
+   })
+});
+
+app.get('/registreren', function (req, res) {
+   res.render('pages/registreren', {
+       title: 'registreren',
+   })
+});
+
+
+app.get('*', function (req, res) {
+   res.status(404).render('pages/404', {
+       url: req.url,
+       title: 'Error 404',
+   })
+});
 
 
 // Express luisterd naar port 3000
 app.listen(PORT, () => {
-   console.log(`http://localhost:${PORT}`)
-})
+console.log(`http://localhost:${PORT}`)
+});
